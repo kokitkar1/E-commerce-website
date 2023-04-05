c2be8c9aa682f2c3653d6c34a03c5f52743ad182
kokitkar1 <skokitkar500@gmail.com>
2023-04-06 05:23:38 +0530

    Final Updated

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import authRoutes from "./routes/authRoute.js"
 import categoryRoutes from './routes/categoryRoutes.js'
 import cors from "cors"
 import productRoutes from './routes/productRoutes.js'
+import path from 'path'
 
 //Configure .env file
 dotenv.config();
@@ -26,16 +27,22 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes)
 
-// Rest API
-app.get('/' , (req,res)=>{
-    res.send("<h1> welcome to ecom app </h1>")
+
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
+
+// Rest API
+// app.get('/' , (req,res)=>{
+//     res.send("<h1> welcome to ecom app </h1>")
+// })
 
 // PORT
 const PORT = process.env.PORT;
